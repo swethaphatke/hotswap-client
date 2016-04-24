1c093879b4393b7b806d1f129bdb4bb3004f819d
pswetha10 <pswetha10@gmail.com>
2016-04-24 20:17:53 +0530

    changed remote asset to bower_assets enabled download of file from server

--- views/js/app.js
+++ views/js/app.js
@@ -1,14 +1,33 @@
-var app = angular.module('hotswapApp', ['ngMaterial', 'ui.router']);
+var app = angular.module('hotswapApp', ['ngMaterial', 'ui.router', 'ngStorage']);
 var request = require('request');
+var fs = require("fs");
 
 app.controller("AppCtrl", function ($scope) {
     //   electron.dialog.showErrorBox('Oppps!','Looks like I messed something up...');
 });
 
-app.controller('PatientListController', function ($scope, $http,$cookies) {
+app.controller('PatientListController', function ($scope, $http, $localStorage, $mdToast) {
+    $scope.$storage = $localStorage.$default({
+        version: 0
+    });
+    console.log($localStorage.version);
     request.get('http://localhost:3000/version', function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var version = JSON.parse(body).version;
+            if (version > $localStorage.version) {
+                var toast = $mdToast.simple();
+                toast.textContent('Update Available, Your app will be upgraded soon');
+                toast.hideDelay(6000);
+                $mdToast.show(toast);
+            }
+
+            request("http://localhost:3000/getUpdated").
+            pipe(fs.createWriteStream('update.zip')).
+            on('close', function () {
+                console.log('File written!');
+            });
+
+
         }
     });
     $http.get("views/data/patient-list.json").then(function (response) {
