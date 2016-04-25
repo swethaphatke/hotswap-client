d6c0a37079d6266140b7ca7a1ce2c8a32276d01f
Vinay Bedre <vinaybedre@gmail.com>
2016-04-25 15:40:43 +0530

    enabled semantic-ui

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/views/ui-views/login.html');
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
